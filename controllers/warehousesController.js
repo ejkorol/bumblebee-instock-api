e0796fe354561b162ed52a480e805728e840ef6d
Jay <janghyeongjae@Jays-MacBook-Pro-2.local>
2024-06-17 17:28:05 -0400

    ticket 15 finish

--- controllers/warehousesController.js
+++ controllers/warehousesController.js
@@ -10,3 +10,27 @@ export const findOne = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const postWarehouse = async (req, res) => {
+  try {
+    const warehouseIds = await knex("warehouses").insert(req.body);
+
+    const warehouseArray = await knex("warehouses").where({
+      id: warehouseIds[0],
+    });
+    const returnWarehouse = warehouseArray[0];
+    return res.status(201).json({
+      id: returnWarehouse.id,
+      warehouse_name: returnWarehouse.warehouse_name,
+      address: returnWarehouse.address,
+      city: returnWarehouse.city,
+      country: returnWarehouse.country,
+      contact_name: returnWarehouse.contact_name,
+      contact_position: returnWarehouse.contact_position,
+      contact_phone: returnWarehouse.contact_phone,
+      contact_email: returnWarehouse.contact_email,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- routes/warehousesRoutes.js
+++ routes/warehousesRoutes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 import * as warehouseController from "../controllers/warehousesController.js";
 
 router.route("/:id").get(warehouseController.findOne);
-
+router.route("/").post(warehouseController.postWarehouse);
 export default router;
