ab7887d5f20ffce07cda3237b99626aeec91fe7f
John Park <johnparkbluejay@gmail.com>
2024-06-18 14:16:44 -0400

    put request final commit

--- routes/warehousesRoutes.js
+++ routes/warehousesRoutes.js
@@ -2,5 +2,8 @@ import express from "express";
 const router = express.Router();
 import * as warehouseController from "../controllers/warehousesController.js";
 
-router.route("/:id").get(warehouseController.findOne).put(warehouseController.putWarehouse);
+router
+	.route("/:id")
+	.get(warehouseController.findOne)
+	.put(warehouseController.putWarehouse);
 export default router;
