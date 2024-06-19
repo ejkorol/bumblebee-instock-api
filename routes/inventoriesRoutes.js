6631376801593e95cefddfc228dcd6dc6e749263
Jason Korol <ejkorol@gmail.com>
2024-06-19 14:59:55 -0400

    reset inventory service and controller

--- routes/inventoriesRoutes.js
+++ routes/inventoriesRoutes.js
@@ -1,13 +1,5 @@
 import express from "express";
 
-import {
-  getInventoryById
-} from "../controllers/inventoryController.js";
-
 const inventoryRouter = express.Router();
-
-inventoryRouter
-  .route("/:id/inventories")
-  .get(getInventoryById);
 
 export default inventoryRouter;
