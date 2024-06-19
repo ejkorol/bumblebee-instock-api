409784528685d8dd3fe8700a30f6973e1d31490b
Jay <janghyeongjae@Jays-MacBook-Pro-2.local>
2024-06-19 11:27:27 -0400

    commit for organizing the folder structures and files

--- routes/warehousesRoutes.js
+++ routes/warehousesRoutes.js
@@ -2,16 +2,17 @@ import express from "express";
 const router = express.Router();
 import * as warehouseController from "../controllers/warehousesController.js";
 
+// router for find one, put and delete controllers
 router
-	.route("/:id")
-	.get(warehouseController.findOne)
-	.put(warehouseController.putWarehouse)
+  .route("/:id")
+  .get(warehouseController.findOne)
+  .put(warehouseController.putWarehouse)
   .delete(warehouseController.deleteWarehouse);
 
-router
-  .route("/details/:id")
-  .get(warehouseController.getWarehouseInventory);
+// router for getWarehouseInventory controller
+router.route("/details/:id").get(warehouseController.getWarehouseInventory);
 
+//router for getWarehouses and postWarehouse controllers
 router
   .route("/")
   .get(warehouseController.getWarehouses)
