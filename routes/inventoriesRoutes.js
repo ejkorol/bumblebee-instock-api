e2f1a5b619fe4403516f63d6f4d5fad71e13bb82
Jason Korol <ejkorol@gmail.com>
2024-06-19 15:34:01 -0400

    create inventory routes

--- routes/inventoriesRoutes.js
+++ routes/inventoriesRoutes.js
@@ -1,5 +1,12 @@
 import express from "express";
+import {
+  postInventory
+} from "../controllers/inventoryController.js";
 
 const inventoryRouter = express.Router();
+
+inventoryRouter
+  .route("/")
+  .post(postInventory)
 
 export default inventoryRouter;
