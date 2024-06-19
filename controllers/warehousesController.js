175168564aa856274df43bb030f95bcd8ee81e33
Jason Korol <ejkorol@gmail.com>
2024-06-19 15:00:34 -0400

    refactor controller logic for streamline reading

--- controllers/warehousesController.js
+++ controllers/warehousesController.js
@@ -1,16 +1,17 @@
 import {
-  getOne,
-  getWarehouseInven,
-  getAll,
-  createOne,
-  updateOne,
-  deleteOne,
+  findOne as findOneService,
+  getWarehouseInventory as getWarehouseInventoryService,
+  getWarehouses as getWarehousesService,
+  postWarehouse as postWarehouseService,
+  putWarehouse as putWarehouseService,
+  deleteWarehouse as deleteWarehouseService,
+  getInventoryById as getInventoryByIdService
 } from "../services/warehouseKnex.js";
 
 /* FIND A WAREHOUSE */
 export const findOne = async (req, res) => {
   try {
-    const warehouse = await getOne(req.params.id);
+    const warehouse = await findOneService(req.params.id);
 
     return res.status(200).json(warehouse);
   } catch (error) {
@@ -21,7 +22,7 @@ export const findOne = async (req, res) => {
 /* GET WAREHOUSE + ITS INVENTORY */
 export const getWarehouseInventory = async (req, res) => {
   try {
-    const result = await getWarehouseInven(req.params.id);
+    const result = await getWarehouseInventoryService(req.params.id);
 
     return res.status(200).json(result);
   } catch (error) {
@@ -32,7 +33,7 @@ export const getWarehouseInventory = async (req, res) => {
 /* GET ALL WAREHOUSES */
 export const getWarehouses = async (_req, res) => {
   try {
-    const warehouses = await getAll();
+    const warehouses = await getWarehousesService();
     res.json(warehouses);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -42,7 +43,7 @@ export const getWarehouses = async (_req, res) => {
 /* CREATE A WAREHOUSE */
 export const postWarehouse = async (req, res) => {
   try {
-    const result = await createOne(req.body);
+    const result = await postWarehouseService(req.body);
     return res.status(201).json(result);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -52,7 +53,7 @@ export const postWarehouse = async (req, res) => {
 /* UPDATE A WAREHOUSE */
 export const putWarehouse = async (req, res) => {
   try {
-    const result = await updateOne(req.params.id, req.body);
+    const result = await putWarehouseService(req.params.id, req.body);
     return res.status(201).json(result);
   } catch (error) {
     res.status(404).json({ message: error.message });
@@ -62,9 +63,20 @@ export const putWarehouse = async (req, res) => {
 /* DELETE A WAREHOUSE */
 export const deleteWarehouse = async (req, res) => {
   try {
-    await deleteOne(req.params.id);
+    await deleteWarehouseService(req.params.id);
     return res.status(204).json();
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
+};
+
+/* GET INVENTORY BY ID */
+export const getInventoryById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const inventory = await getInventoryByIdService(id);
+    res.status(200).json(inventory);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  };
 };
