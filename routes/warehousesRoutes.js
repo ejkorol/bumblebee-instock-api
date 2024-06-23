b9183ad7bb31ee865ad4bc70dd2ded6218ea0ea7
Jason Korol <ejkorol@gmail.com>
2024-06-23 15:19:57 -0400

    format routes

--- routes/warehousesRoutes.js
+++ routes/warehousesRoutes.js
@@ -12,14 +12,24 @@ import {
 const router = express.Router();
 
 /* WAREHOUSE BY ID */
-router.route("/:id").get(findOne).put(putWarehouse).delete(deleteWarehouse);
+router
+  .route("/:id")
+  .get(findOne)
+  .put(putWarehouse)
+  .delete(deleteWarehouse);
 
 /* WAREHOUSE DETAILS BY ID */
-router.route("/details/:id").get(getWarehouseInventory);
+router
+  .route("/:id/inventories")
+  .get(getWarehouseInventory);
 
 /* BASE */
-router.route("/").get(getWarehouses).post(postWarehouse);
+router
+  .route("/")
+  .get(getWarehouses)
+  .post(postWarehouse);
 
-router.route("/:id/inventories").get(getInventoryById);
+// router.route("/:id/inventories")
+//   .get(getInventoryById);
 
 export default router;
